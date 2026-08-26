f94cdf3d0fd9a02ae201b3202c25e4cde55a4c08
nnetr <nnetreba06@gmail.com>
2026-08-26 09:41:58 +0300

    a

--- test.cpp
+++ test.cpp
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <assert.h>
 
+#define EPS 1e-9
+
 #define verify_it(condition, message)\
         if(!(condition)){\
             printf(stderr, "file: %s, lint: %d, fall: %s", __FILE__, __LINE__, message);\
@@ -12,38 +14,40 @@ double is_it_dot(double a, double b, double c, double x){
     return a*x*x + b*x + c;
 }
 
-bool is_it_in(double xv, double yv){
-    long X = txGetExtentX();
-    long Y = txGetExtentY();
-    if (xv < 0 || xv > X)
-        return false;
-    if (yv < 0 || yv > Y)
-        return false;
-    return true;
+bool is_zero(const double val) {
+  return fabs(val) < EPS;
 }
 
-void set_scale(double* scale, double a, double b, double c, double xv, double yv){
-    while(!is_it_in(xv * *scale, yv * *scale)){
-        *scale /= 5;
+void calculate_scale(double xv, double yv, double pX, double pY, double* scaleX, double* scaleY) {
+    double maxX = pX * 0.65;
+    double maxY = pY * 0.65;
+
+    if (!is_zero(fabs(xv))) {
+        *scaleX = maxX / fabs(xv);
+    }
+
+    if (!is_zero(fabs(yv))) {
+        *scaleY = maxY / fabs(yv);
     }
 }
 
 int main(){
-    double a = 1, b = 100, c = 100;
+    double a = 1, b = 0, c = 2;
     double xv = -b / (2 * a), yv = is_it_dot(a, b, c, xv);
-    double scale = 1;
     POINT sizeOfWindow = txGetExtent();
     double pX = sizeOfWindow.x / 2, pY  = sizeOfWindow.y / 2;
-    set_scale(&scale, a, b, c, xv + pX, -yv + pY);
+    double scaleX = 1.0;
+    double scaleY = 1.0;
+    calculate_scale(xv, yv, pX, pY, &scaleX, &scaleY);
     txCreateWindow(sizeOfWindow.x, sizeOfWindow.y);
     txClearConsole();
     txSetColor(TX_WHITE, 1);
-    txLine(0, sizeOfWindow.y / 2, sizeOfWindow.x, sizeOfWindow.y / 2);
-    txLine(sizeOfWindow.x / 2, 0, sizeOfWindow.x / 2, sizeOfWindow.y);
+    txLine(0, pY, sizeOfWindow.x, pY);
+    txLine(pX, 0, pX, sizeOfWindow.y);
 
-    for(double x = -(1920 / 2); x < (1920 / 2); x += 0.08){
+    for(double x = -pX; x < pX; x += 0.05){
         double y = is_it_dot(a, b, c, x);
-        txSetPixel(x * scale + pX, -y * scale + pY, TX_PINK);
+        txSetPixel(x * scaleX + pX, -y * scaleY + pY, RGB(0, 191, 255));
     }
 }
 
